51f254c569f4350dfe6e2536ba6269afb8441276
Pritom Karmokar <71670082+PritomKarmokar@users.noreply.github.com>
2025-05-17 00:10:35 +0600

    updated: setup.sh

--- init_django_project.sh
+++ init_django_project.sh
@@ -38,8 +38,8 @@ cat > .env.example <<EOL
 DEBUG=True
 SECRET_KEY='$SECRET_KEY'
 DB_NAME=your_db_name
-DB_USER=your_db_user
-DB_PASSWORD=your_db_password
+DB_USER=postgres
+DB_PASSWORD=postgres
 DB_HOST=localhost
 DB_PORT=5432
 ALLOWED_HOSTS=localhost,127.0.0.1
